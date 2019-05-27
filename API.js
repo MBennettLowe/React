6dc7be8005bd52b328ad06fb8e969e7fbb88988f
mbennettlowe <mbennettlowe@gmail.com>
2019-05-27 09:55:06 -0400

    MBL

---
+++ API.js
@@ -0,0 +1,28 @@
+//https://jsonplaceholder.typicode.com/posts
+
+//materialui.com
+
+import React, { Component } from 'react'
+
+class Data extends Component t
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: []
+        }
+    }
+
+componentDidMount () {
+    fetch('https')
+    .then result => result.json())
+    .then res => this.setState { items: res })
+}
+    render() {
+        return {
+            <div>
+                
+            </div>
+    }
+}
+}
+export default API
